1bad9690ec9f212d7b2f19c346bb392054bdc584
Pedro Escapalete <pedroescapalete@gmail.com>
2026-03-25 16:52:34 -0300

    add filmes e series classicas para o React Movie

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ const FILMES_DADOS = [
         id: '1',
         titulo: 'A Empregada',
         autor: 'Freida McFadden',
-        preco: 'Rs. 780',
+        preco: 'R$ 45,90',
+        descricao: 'A Empregada é um romance de suspense psicológico que segue a história de uma jovem mulher que aceita um emprego como empregada doméstica em uma mansão isolada. À medida que ela se envolve mais na vida da família para a qual trabalha, segredos obscuros começam a emergir, colocando sua própria segurança em risco.',
         tags: ['Design', 'User Interface'],
         imagem: 'https://m.media-amazon.com/images/I/81qjGN2mqPL._UF1000,1000_QL80_.jpg',
     },
@@ -18,7 +19,8 @@ const FILMES_DADOS = [
         id: '2',
         titulo: 'Grey´s Anatomy',
         autor: 'HULU',
-        preco: 'Rs. 780',
+        preco: 'R$ 25,90',
+        descricao: 'Grey´s Anatomy é uma série de televisão americana que segue a vida de médicos e residentes do fictício Hospital Seattle Grace. A série é conhecida por seus personagens complexos, tramas emocionantes e momentos dramáticos, explorando temas como amor, amizade, ética médica e desafios profissionais.',
         tags: ['Design', 'User Interface'],
         imagem: 'https://m.media-amazon.com/images/M/MV5BYTVjNWVhYTctMGJkMS00NWFjLWE2N2QtNmQ1Y2FhZDFkNzUwXkEyXkFqcGc@._V1_.jpg',
     },
@@ -26,10 +28,38 @@ const FILMES_DADOS = [
         id: '3',
         titulo: 'Magic Mickey - The Movie',
         autor: 'Disney',
-        preco: 'Rs. 780',
+        preco: 'R$ 59,90',
+        descricao: 'Mickey é um camundongo que vive em um mundo mágico e encantado. Ele é conhecido por sua personalidade alegre, otimista e aventureira. Mickey é um personagem icônico da Disney e tem sido amado por gerações de fãs ao redor do mundo.',
         tags: ['Design', 'User Interface'],
         imagem: 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2022/09/15/1885838941-mickey-a-historia-de-um-camundongo-poster-disney-plus.jpg',
     },
+    {
+        id: '4',
+        titulo: 'Braddock',
+        autor: 'Paramount+',
+        preco: 'R$ 25,90',
+        descricao: 'Braddock é um filme de ação e aventura que segue a história de um soldado que se vê envolvido em uma missão perigosa. O filme é conhecido por suas cenas de ação intensas e sua narrativa envolvente.',
+        tags: ['Action', 'Adventure'],
+        imagem: 'https://m.media-amazon.com/images/I/71HtYr5OlzL._AC_SL1000_.jpg',
+    },
+    {
+        id: '5',
+        titulo: 'Rambo Programado para Matar',
+        autor: 'Paramount+',
+        preco: 'R$ 25,90',
+        descricao: 'Rambo Programado para Matar é um filme de ação e aventura que segue a história de um ex-soldado que se vê envolvido em uma missão perigosa. O filme é conhecido por suas cenas de ação intensas e sua narrativa envolvente.',
+        tags: ['Action', 'Adventure'],
+        imagem: 'https://m.media-amazon.com/images/M/MV5BYzc4ZmQ5YzEtYTQzYy00ZDdiLWI2NmQtZTA3MzM1ZGIyYmUzXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    },
+    {
+        id: '6',
+        titulo: 'Texas Walker Ranger',
+        autor: 'Paramount+',
+        preco: 'R$ 25,90',
+        descricao: 'Texas Walker Ranger é uma série de ação e crime centrada em Cordell Walker (Chuck Norris), um Texas Ranger de elite que combate o crime em Dallas utilizando artes marciais e métodos tradicionais. Com um forte código moral, ele e seu parceiro Jimmy Trivette resolvem casos difíceis, misturando faroeste moderno com investigações.',
+        tags: ['Action', 'Adventure'],
+        imagem: 'https://m.media-amazon.com/images/I/91b4KhYE-aL._AC_SL1500_.jpg',
+    }
 ];
 
 const CardFilme = ({ item }) => {
@@ -47,6 +77,9 @@ const CardFilme = ({ item }) => {
                 </Text>
                 <Text style={styles.autor}>{item.autor}</Text>
                 <Text style={styles.preco}>{item.preco}</Text>
+                <Text style={styles.descricao} numberOfLines={3}>
+                    {item.descricao}
+                </Text>
 
                 <View style={styles.tagsContainer}>
                     {item.tags.map((tag, index) => (
@@ -230,6 +263,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 10,
     },
+
+    descricao: {
+        color: '#8A8CB2',
+        fontSize: 12,
+        marginBottom: 10,
+    },
+
     tagsContainer: {
         flexDirection: 'row',
         flexWrap: 'wrap',
